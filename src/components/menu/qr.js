0758a63f3debb26307e39639c0f024a8e5a1a3a6
burakpols <burakpolsq@gmail.com>
2024-03-01 22:51:10 +0300

    loading and smooth transition

--- src/components/menu/qr.js
+++ src/components/menu/qr.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import "./qr.css"; // Import the CSS file
-import axios from 'axios';
+import axios from "axios";
 
 const Qr = () => {
   const [items, setItems] = useState([]);
   const [menuItems, setMenuItems] = useState([]);
   const [categories, setCategories] = useState(["Hepsi"]);
+  const [loading, setLoading] = useState(true); // Add loading state
   const mainUrl = "https://mihman-be.vercel.app"; // Define main URL
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -16,7 +18,9 @@ const Qr = () => {
         const response = await axios.get(`${mainUrl}/items`);
         setItems(response.data);
       } catch (error) {
-        console.error('Error fetching items:', error);
+        console.error("Error fetching items:", error);
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or failure
       }
     };
 
@@ -24,14 +28,24 @@ const Qr = () => {
   }, []);
 
   useEffect(() => {
-    const allCategories = ["Hepsi", ...new Set(items.map((item) => item.category))];
+    const allCategories = [
+      "Hepsi",
+      ...new Set(items.map((item) => item.category)),
+    ];
     setCategories(allCategories);
     setMenuItems(items);
   }, [items]);
 
   const filterItems = (category) => {
-    const newItems = (category === "Hepsi") ? items : items.filter((item) => item.category === category);
-    setMenuItems(newItems);
+    const newItems =
+      category === "Hepsi"
+        ? items
+        : items.filter((item) => item.category === category);
+    setIsTransitioning(true); // Start transition
+    setTimeout(() => {
+      setMenuItems(newItems);
+      setIsTransitioning(false); // End transition after a short delay
+    }, 300); // Adjust the delay as needed for your transition duration
   };
 
   return (
@@ -42,7 +56,13 @@ const Qr = () => {
           <div className="underline" />
         </div>
         <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
+        <div className={`menu-items ${isTransitioning ? 'transitioning' : ''}`}>
+          {loading ? (
+            <div className="loader"></div>
+          ) : (
+            <Menu items={menuItems} />
+          )}
+        </div>
       </section>
     </main>
   );
